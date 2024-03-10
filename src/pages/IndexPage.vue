a6006be0c7f7426b261ec5953a82b16fcc64b117
Isf20 <Ilada.isf.com>
2024-03-10 19:06:50 +0700

    jest-test

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Vue is not awesome.',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
